3ce90c2844c373056835d652a1f83dbfad14b015
Bekker Barnabás <bekker.barnabas@ckik.loc>
2025-12-12 09:54:12 +0100

    beallitas

---
+++ adventofcode/2.nap/feladat.py
@@ -0,0 +1,33 @@
+f=open("be.txt")
+t=f.read()
+f.close()
+ertekek=[]
+for i in t:
+    i=i.strip()
+    ertek=(i)
+    ertekek.append(ertek)
+t=[]
+ertek=""
+for i in range(len(ertekek)):
+    if str(ertekek[i])!="-" and str(ertekek[i])!=",":
+        ertek+=str(ertekek[i])
+    else:
+        t.append(str(ertek))
+        ertek=""
+t.append(str(ertek))
+print(t)
+
+ossz=0
+for i in range(0, len(t), 2):
+    for j in range(int(t[i]), int(t[i+1])+1, 1):
+        ertek=str(j)
+        if len(ertek)%2==0:
+            if ertek[:round(len(ertek)/2)]==ertek[round(len(ertek)/2):]:
+                print(ertek)
+                ossz+=int(ertek)
+print(ossz)
+            
+# ertek="1534"
+# hossz=round(len(ertek)/2)
+# print(hossz)
+# print(ertek[:hossz])
