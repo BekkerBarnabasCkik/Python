261243cff7fe3c579348f0be6e557f15c5f8240e
Bekker Barnabas <bekker.barnabas@ckik.hu>
2026-01-15 17:37:22 +0100

    7.nap

--- adventofcode/7.nap/feladat.py
+++ adventofcode/7.nap/feladat.py
@@ -17,14 +17,43 @@ def hosszmeghatarozas(tömb):
     return len(t)-1
 
 def main(fajl):
-    ertekek=Fajlbeolvasas("proba.txt")
+    ertekek=Fajlbeolvasas(fajl)
     hossz=ertekek[0]
     t=ertekek[1]
+    feladat(hossz, t)
 
-    print(hossz)
-    print(t)
+def SMeghatarozas(tömb):
+    i=0
+    while tömb[i]!="S":
+        i+=1
 
-main("proba.txt")
+    return i
 
-# def feladat():
+def Lepesek(Spoz, tömb, hossz):
+    db=0
+    tömb[Spoz+hossz]="i"
+    for i in range(hossz, len(tömb)-hossz, 1):
+        if tömb[i]=="i":
+            if tömb[i+hossz]=="^":
+                if tömb[i+hossz-1]!="^":
+                    tömb[i+hossz-1]="i"
+                if tömb[i+hossz+1]!="^":
+                    tömb[i+hossz+1]="i"
+                db+=1
+            else:
+                tömb[i+hossz]="i"
+    return db
 
+def kiiras(tömb, hossz):
+    print(tömb[0], end="")
+    for i in range(1, len(tömb), 1):
+        if (i+1)%hossz!=0:
+            print(tömb[i], end="")
+        else:
+            print(tömb[i], end="\n")
+
+def feladat(hossz, tömb):
+    Spoz=SMeghatarozas(tömb)
+    print(Lepesek(Spoz, tömb, hossz))
+
+main("be.txt")
--- függvény/feladat.py
+++ függvény/feladat.py
@@ -1,60 +1,40 @@
-# import random
-
-# hossz=int(input())
-# mettol=int(input())
-# meddig=int(input())
-
-# def kiiras(t):
-#     for i in range(0, len(t), 1):
-#         if i!=len(t)-1:
-#             print(t[i], end=",")
-#         else:
-#             print(t[i])
-
-# def tombfeltoltes(n, a, b):
-#     t=[]
-#     for i in range(n):
-#         t.append(random.randint(a, b))
-    
-#     return t
-
-# t=tombfeltoltes(hossz, mettol, meddig)
-# kiiras(t)
-
-#----------------------11.Feladat------------------------
 import random
-
 n=int(input())
-t=[0]
 
-def tombfeltoltes(n, t):
-    for i in range(n-2):
-        t.append(random.randint(1, 1000))
-    t.append(0)
-
+def TombFeltoltes(hanyszor, mettol, meddig):
+    t=[]
+    for i in range(hanyszor):
+        t.append(random.randint(mettol, meddig))
+    
     return t
 
-magassagok=tombfeltoltes(n, t)
+def Megszamlalas(tömb, mettol, meddig):
+    alkalmak=[]
+    for i in range(mettol, meddig+1, 1):
+        db=0
+        for j in range(len(tömb)):
+            if tömb[j]==i:
+                db+=1
+        alkalmak.append(db)
 
-def maximum(tömb):
+    return alkalmak
+
+def MaximumKivalasztas(tömb):
     maxi=0
-    for i in range(len(tömb)):
+    for i in range(1, len(tömb), 1):
         if tömb[i]>tömb[maxi]:
             maxi=i
     
     return maxi
 
-maxi=maximum(magassagok)
+def main(n):
+    ki=TombFeltoltes(n, 1, 20)
+    print(ki)
+    kit=TombFeltoltes(n, 1, 10)
+    print(kit)
+    alkalmak=Megszamlalas(kit, 1, 10)
+    print(alkalmak)
+    print(MaximumKivalasztas(alkalmak)+1)
 
-def kiiras(tömb):
-    for i in range(len(tömb)):
-        if i!=len(t)-1:
-            print(t[i], end=",")
-        else:
-            print(t[i])
-
-kiiras(magassagok)
-print()
-print(f"A legmagasabb pont a {maxi+1}. helyen volt")
-
+main(n)
 
