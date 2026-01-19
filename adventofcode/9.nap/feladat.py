0fb909fdec32fc5669d8e759715a7a49a311013c
Bekker Barnabás <bekker.barnabas@ckik.loc>
2026-01-19 13:18:58 +0100

    9.nap/1

---
+++ adventofcode/9.nap/feladat.py
@@ -0,0 +1,44 @@
+import math
+
+def fajlbeovlasas(fajlNev):
+    f=open(fajlNev)
+    t=f.read()
+    f.close()
+    ertekek=[]
+    ertek=""
+    x=[]
+    y=[]
+    for i in t:
+        i=i.strip()
+        if i!="," and i!="":
+            ertek+=i
+        elif i=="," and ertek!="":
+            x.append(int(ertek))
+            ertek=""
+        elif ertek!="":
+            y.append(int(ertek))
+            ertek=""
+    y.append(int(ertek))
+
+    return x, y 
+
+def tavolsag(x, y, i, j):
+    return (abs(x[j]-x[i])+1)*(abs(y[j]-y[i])+1)
+
+def Kivalasztas(x, y):
+    maxter=0
+    for i in range(len(x)-1):
+        for j in range(i+1, len(x), 1):
+            ter=tavolsag(x, y, i, j)
+            if ter>maxter:
+                maxter=ter
+                print(maxter)
+    print(maxter)
+
+
+
+ertekek=fajlbeovlasas("be.txt")
+x=ertekek[0]
+y=ertekek[1]
+Kivalasztas(x, y)
+
---
+++ adventofcode/9.nap/proba.py
@@ -0,0 +1,9 @@
+import math
+
+x1=float(input())
+x2=float(input())
+y1=float(input())
+y2=float(input())
+
+d=math.sqrt((math.pow((x2-x1), 2)+math.pow((y2-y1), 2)))
+print(d)
