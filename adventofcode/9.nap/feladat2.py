c6e2c35aaa2101c6610850726bb0d21b023fdf47
Bekker Barnabas <bekker.barnabas@ckik.hu>
2026-01-22 16:21:45 +0100

    9/2

--- adventofcode/9.nap/feladat2.py
+++ adventofcode/9.nap/feladat2.py
@@ -27,24 +27,63 @@ def tavolsag(x, y, i, j):
 
 
 
-def eldonters(joy, jox, x, y):
-    metszesek=0
-    i=0
-    for i in range(len(x)):
-        Ax=x[i]
-        Ay=y[i]
-        Bx=x[(i+1)%len(x)]
-        By=y[(i+1)%len(x)]
-        if (Ay > joy and By <= joy) or (Ay <= joy and By > joy):
-            x_metszes=Ax+(joy-Ay)*(Bx-Ax)/(By-Ay)
-            if x_metszes>jox:
-                metszesek+=1
+# def eldonters(joy, jox, x, y):
+#     metszesek=0
+#     i=0
+#     for i in range(len(x)):
+#         Ax=x[i]
+#         Ay=y[i]
+#         Bx=x[(i+1)%len(x)]
+#         By=y[(i+1)%len(x)]
+#         if (Ay > joy and By <= joy) or (Ay <= joy and By > joy):
+#             x_metszes=Ax+(joy-Ay)*(Bx-Ax)/(By-Ay)
+#             if x_metszes>jox:
+#                 metszesek+=1
     
-    if metszesek%2!=0:
-        return True
-    else:
-        return False
+#     if metszesek%2!=0:
+#         return True
+#     else:
+#         return False
+
+# def balFelso(jox, joy, x, y):
+#     return (jox>=x and joy<=y)
+
+# def jobbFelso(jox, joy, x, y):
+#     return (jox<=x and joy<=y)
+
+# def balAlso(jox, joy, x, y):
+#     return (jox>=x and joy>=y)
+
+# def jobbAlso(jox, joy, x, y):
+#     return (jox<=x and joy>=y)
+
+# def Eldontes(jox, joy, x, y):
+#     egy=False
+#     ketto=False
+#     harom=False
+#     negy=False
+#     osszes=False
+#     i=0
+#     db=0
+#     while i<len(x) and osszes==False:
+#         if egy==False and balFelso(jox, joy, x[i], y[i]):
+#            egy=True
+#            db+=1
+#         if ketto==False and jobbFelso(jox, joy, x[i], y[i]):
+#             ketto=True
+#             db+=1
+#         if harom==False and balAlso(jox, joy, x[i], y[i]):
+#            harom=True
+#            db+=1
+#         if negy==False and jobbAlso(jox, joy, x[i], y[i]):
+#             negy=True
+#             db+=1
+#         if db==4:
+#             osszes=True
+#         i+=1
     
+    # return osszes
+
 
 def Kivalasztas(x, y):
     maxter=0
@@ -54,21 +93,18 @@ def Kivalasztas(x, y):
             if ter>maxter:
                 jox=[x[i], x[j]]
                 joy=[y[i], y[j]]
-                belül=True
-                print(jox, joy)
-                for k in range(2):
-                    if eldonters(joy[k], jox[len(jox)-(k+1)], x, y) and belül==True:
-                        belül=True
-                    else:
-                        belül=False
-                if belül:
-                    maxter=ter
-                    print(maxter)
+                # k=0
+                # while k<2 and Eldontes(jox[k], joy[len(joy)-(k+1)], x, y):
+                #     k+=1
+                # if k==2:
+                #     maxter=ter
+                #     print(maxter)
+                    
     print(maxter)
 
 
 
-ertekek=fajlbeovlasas("proba.txt")
+ertekek=fajlbeovlasas("be.txt")
 x=ertekek[0]
 y=ertekek[1]
 Kivalasztas(x, y)
