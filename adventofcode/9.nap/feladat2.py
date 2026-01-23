b676879533590d39173fe2a14e2a9e4047a56236
Bekker Barnabás <bekker.barnabas@ckik.loc>
2026-01-23 09:48:43 +0100

    9/2 suli

--- adventofcode/9.nap/feladat2.py
+++ adventofcode/9.nap/feladat2.py
@@ -25,24 +25,85 @@ def fajlbeovlasas(fajlNev):
 def tavolsag(x, y, i, j):
     return (abs(x[j]-x[i])+1)*(abs(y[j]-y[i])+1)
 
+
+
+def eldonters(joy, jox, x, y):
+    metszesek=0
+    i=0
+    for i in range(len(x)):
+        Ax=x[i]
+        Ay=y[i]
+        Bx=x[(i+1)%len(x)]
+        By=y[(i+1)%len(x)]
+        if (Ay > joy and By <= joy) or (Ay <= joy and By > joy):
+            x_metszes=Ax+(joy-Ay)*(Bx-Ax)/(By-Ay)
+            if x_metszes>jox:
+                metszesek+=1
+    
+    if metszesek%2!=0:
+        return True
+    else:
+        return False
+    
+
 def Kivalasztas(x, y):
     maxter=0
-    for i in range(len(x)-1):
+    for i in range(len(x)):
         for j in range(i+1, len(x), 1):
             ter=tavolsag(x, y, i, j)
             if ter>maxter:
-                maxter=ter
-                print(maxter)
+                jox=[x[i], x[j]]
+                joy=[y[i], y[j]]
+                belül=True
+                print(jox, joy)
+                for k in range(2):
+                    if eldonters(joy[k], jox[len(jox)-(k+1)], x, y) and belül==True:
+                        belül=True
+                    else:
+                        belül=False
+                if belül:
+                    maxter=ter
+                    print(maxter)
     print(maxter)
 
 
 
-ertekek=fajlbeovlasas("be.txt")
+ertekek=fajlbeovlasas("proba.txt")
 x=ertekek[0]
 y=ertekek[1]
 Kivalasztas(x, y)
 
 
-# def eldonters(x1, x2, y1, y2):
-#     while 
+
+
+
+
+# FÜGGVÉNY Pont_Benne_Van_E_Sokszögben(
+#     PONT_X, PONT_Y,
+#     SOKSZÖG_X, SOKSZÖG_Y
+# ):
+
+#     metszések = 0
+#     n = SOKSZÖG_X hossza
+
+#     CIKLUS i = 0-tól n-1-ig:
+
+#         Ax = SOKSZÖG_X[i]
+#         Ay = SOKSZÖG_Y[i]
+
+#         Bx = SOKSZÖG_X[(i + 1) mod n]
+#         By = SOKSZÖG_Y[(i + 1) mod n]
+
+#         HA (Ay > PONT_Y) != (By > PONT_Y):
+
+#             x_metszés =
+#                 Ax + (PONT_Y - Ay) * (Bx - Ax) / (By - Ay)
+
+#             HA PONT_X < x_metszés:
+#                 metszések = metszések + 1
+
+#     HA metszések páratlan:
+#         VISSZA true
+#     KÜLÖNBEN:
+#         VISSZA false
 
--- alapvizsga/Gyakorlás 3/Források/Programozás/feladat.py
+++ alapvizsga/Gyakorlás 3/Források/Programozás/feladat.py
@@ -12,10 +12,67 @@ print(f"Béla vagyok, szerencse számám a(z) {szerencse()}, kedvenc színem a p
 
 #2.feladat
 
-nev=str(input())
-szam=random.randint(2, 12)
-t=[]
+
 def tabla(t, szam):
     t=[szam]
     for i in range(9):
         t.append(szam+(i+1)*5)
+
+    return t
+
+def Osszegzes(t):
+    ertek=0
+    for i in range(len(t)):
+        if t[i]%3==0:
+            ertek+=t[i]
+    
+    return ertek
+
+def main()
+    nev=str(input())
+    szam=random.randint(2, 12)
+    print=(f"név: {nev}")
+    print(f"Felírt számok: {tabla(szam)}")
+    print(Osszegzes(tabla(szam)))
+
+# 3.feladat
+
+    def Beolvasas(fajl)
+        f=open(fajl)
+        t=f.read()
+        f.close()
+        ertek=""
+        szamolas=0
+        for i in t:
+            if i!="":
+                ertek+=i
+            elif ertek!="":
+                if szamolas==0:
+                    nevek.append(ertek)
+                    szamolas+=1
+                elif szamolas==1:
+                    gyümölcs.append(ertek)
+                    szamolas+=1
+                else:
+                    szam.append(int(ertek))
+                    szamolas=0
+        szamok.append(ertek)
+
+        return  nevek, gyümölcs, szamolas
+
+    nevek, gyümölcs, szamolas = Beolvasas("diakok.txt")
+
+    infomax=0
+    db=0
+    for i in range(len(nevek)):
+        print(f"{nevek[i]} {gyümölcs[i]} {szamok[i]}")
+        if gyümölcs[i].lover()=="alma"
+            almakedvelok.append(nevek[i])
+        if szamok[i]>szamok[infomax]
+            infomax=i
+        if len(nevek[i])>7:
+            db+=1
+    print(f"Az alma kedvelők: {almakedvelok}")
+    print(f"A legtöbb informatikai órajá egy {nevek[infomax]} ne4vű diáknak volt.")
+    print(f"{db} diáknak hosszabb a neve 7 karakternél")
+    
