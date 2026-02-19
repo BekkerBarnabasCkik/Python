0ef73f0306bc9ca712bb153087a8671a8fbdb984
Bekker Barnabás <bekker.barnabas@ckik.loc>
2026-02-19 07:51:46 +0100

    11/2

--- adventofcode/11.nap/feladat2.py
+++ adventofcode/11.nap/feladat2.py
@@ -28,41 +28,67 @@ def fajlbeolvasas(fajl):
     
     return adatok
 
-def megnezes(adatok, keresett, keresendo, db, tiltott, hova):
+def bennevane(honnan, hova, miben):
+    i=0
+    while i<len(miben) and (miben[i][0]!=honnan and miben[i][1]!=hova):
+        i+=1
+    return i==len(miben)
+
+def UtvonalakNoveles(keresendo, kimenet, utvonalakszama):
+    i=0
+    while i<len(keresendo) and keresendo[i]!=kimenet:
+        i+=1
+    if i<len(keresendo):
+        utvonalakszama[i]+=1
+    else:
+        utvonalakszama.append(utvonalakszama[0])
+    
+    return utvonalakszama
+
+def Megszmolas_Hozzafuzes(kimenetek, i, hova, tiltott, megkeresettek, keresendo, keresett, db, utvonalakszama):
+    if kimenetek[i]!=hova and kimenetek[i]!=tiltott and kimenetek[i]!="out" and kimenetek[i]!=" ":
+        megkeresettek.append((keresett, kimenetek[i]))
+        utvonalakSzama=UtvonalakNoveles(keresendo, kimenetek[i], utvonalakszama)
+        keresendo.append(kimenetek[i])
+    elif kimenetek[i]==hova:
+        db+=1
+    
+    return db, megkeresettek, keresendo
+
+def megnezes(adatok, keresett, keresendo, db, tiltott, hova, megkeresettek, utvonalakszama):
     kimenetek=adatok[keresett]
-    print(adatok)
     for i in range(len(kimenetek)):
-        if kimenetek[i]!=hova and kimenetek[i]!=tiltott and kimenetek[i]!="out" and kimenetek[i]!=" ":
-            keresendo.append(kimenetek[i])
-        elif kimenetek[i]==hova:
-            db+=1
-            print(db)
+        if bennevane(keresett, kimenetek[i], megkeresettek):
+            db, megkeresettek, keresendo=Megszmolas_Hozzafuzes(kimenetek, i, hova, tiltott, megkeresettek, keresendo, keresett, db, utvonalakszama)
     
-    return keresendo, db
+    return keresendo, db, megkeresettek
     
+
+
 def vegigmenes(adatok, kiindulas, tiltott, hova, regidarab):
     keresendo=[]
+    megkeresettek=[]
     db=0
+    utvonalakSzama=[1]
     keresendo=adatok[kiindulas]
     adatok.pop(kiindulas)
     while len(keresendo)!=0:
         keresett=keresendo[0]
         keresendo.pop(0)
-        keresendo, db=megnezes(adatok, keresett, keresendo, db, tiltott, hova)
-        print(len(keresendo))
-    
-    if regidarab>db:
-        return regidarab
-    else:
+        keresendo, db, megkeresettek=megnezes(adatok, keresett, keresendo, db, tiltott, hova, megkeresettek, utvonalakSzama)
+    if db>1:
         return db
+    else:
+        return 0
 
 def lehetoseg1(adatok):
     db=0
-    db=vegigmenes(adatok, "svr", "dac", "fft", db)
+    db+=vegigmenes(adatok, "svr", "dac", "fft", db)
     print(db)
-    db=vegigmenes(adatok, "fft", "out", "dac", db)
+    db+=vegigmenes(adatok, "fft", "out", "dac", db)
     print(db)
-    db=vegigmenes(adatok, "dac", "fft", "out", db)
+    db+=vegigmenes(adatok, "dac", "fft", "out", db)
+    print(db)
 
     return db
 
